753de03d54ed1076b3de862ff561d1b515e5a93b
GoodMorningPeter <shuxuejianmo202202@163.com>
2022-11-30 22:08:39 +0800

    修改一些bug

--- main.asm
+++ main.asm
@@ -17,45 +17,62 @@ include msvcrt.inc
 scanf proto c:dword,:vararg
 printf proto c:dword,:vararg
 
-
 BASE struct
         posx    dd      ?
         posy    dd      ?
         lengthx dd      ?
         lengthy dd      ?
         alive   dd      ?
-        DC      dd      ?  ;ç‰©ä½“å›¾ç‰‡å¥æŸ„
-        rel_v   dd      ?  ;ç›¸å¯¹ç§»åŠ¨é€Ÿåº¦ï¼ˆå€ç‡ï¼‰
+        DC      dd      ?  ;ÎïÌåÍ¼Æ¬¾ä±ú
+        rel_v   dd      ?  ;Ïà¶ÔÒÆ¶¯ËÙ¶È£¨±¶ÂÊ£©
 BASE ends
 
 Subject struct 
         base    BASE  <> 
 Subject ends
+
 MONEY_1                 equ 1000
 MONEY_2                 equ 1001
-PROP_ACC_SELF           equ 2000 ;ç»™è‡ªå·±åŠ é€Ÿ
-PROP_DEC_SELF           equ 2001 ;ç»™è‡ªå·±å‡é€Ÿ
-OBST_HARD               equ 3000 ;ç¡¬éšœç¢
-OBST_SOFT               equ 3001 ;è½¯éšœç¢
+PROP_ACC_SELF           equ 2000 ;¸ø×Ô¼º¼ÓËÙ
+PROP_DEC_SELF           equ 2001 ;¸ø×Ô¼º¼õËÙ
+OBST_HARD               equ 3000 ;Ó²ÕÏ°­
+OBST_SOFT               equ 3001 ;ÈíÕÏ°­
 Targets struct
         base    BASE  <> 
         typeid  dd      ?
-        runwayid dd     ?
 Targets ends
 
 .data?
-player          Subject <>              ;äººç‰©
-bullet          Subject <>              ;å­å¼¹
-targets         Targets  1000 dup(<>)   ;ç‰©ä½“
+player          Subject <>              ;ÈËÎï
+bullet          Subject <>              ;×Óµ¯
+env_objecet_1   BASE    <>              ;Èı¸ö»·¾³ÎïÌå 1
+env_objecet_2   BASE    <>              ;Èı¸ö»·¾³ÎïÌå 2
+env_objecet_3   BASE    <>              ;Èı¸ö»·¾³ÎïÌå 3
+targets         Targets  1000 dup(<>)   ;ÎïÌå
+
+;zzl own
+game_start              BASE <>
+game_intro              BASE <>
+game_start_chosen       BASE <>
+game_intro_chosen       BASE <>
+.data
+choose_start            equ 1 
+choose_intro            equ 2 
+choose_which_char            dd  0
+
+in_begining             equ 0
+in_game                 equ 1
+cur_interface           dd  0
+
 .const 
 MAX_TARGET_NUMBER dd 1000
 .data
-target_number   dd      0               ;ç‰©ä½“æ•°é‡ï¼ˆtargetsæ•°ç»„é•¿åº¦ï¼‰
+target_number   dd      0               ;ÎïÌåÊıÁ¿£¨targetsÊı×é³¤¶È£©
 
 .data
-base_speed      dd      2 ;åŸºå‡†é€Ÿåº¦ å•ä½ä¸ºåƒç´ 
+base_speed      dd      2 ;»ù×¼ËÙ¶È µ¥Î»ÎªÏñËØ
 
-;when you store something-> offset targets + (id%MAX_TARGET_NUMBER) å¾ªç¯é˜Ÿåˆ—
+;when you store something-> offset targets + (id%MAX_TARGET_NUMBER) Ñ­»·¶ÓÁĞ
 
 
 
@@ -64,11 +81,21 @@ base_speed      dd      2 ;åŸºå‡†é€Ÿåº¦ å•ä½ä¸ºåƒç´ 
 ID_TIMER        equ     1
 IDB_BACKG       equ     100
 IDB_OBJ1        equ     101
+IDB_OBJ2        equ     102
+IDB_OBJ3        equ     103
 IDB_ICON        equ     200
-gameH           equ     498
+IDB_GAMESTART   equ     301
+IDB_GAMEINTRO   equ     302
+IDB_GAMESTARTC  equ     303
+IDB_GAMEINTROC  equ     304
+
+
+gameH           equ     548
 gameW           equ     650
-object1H_init   equ     577
-object1W_init   equ     860
+charH           equ     400
+charW           equ     400
+object1H_init   equ     400
+object1W_init   equ     600
 object1H_begin  equ     57
 object1W_begin  equ     86
 object1H_end    equ     57*3
@@ -84,134 +111,166 @@ object_move_v    dd     2
 .data?
 hInstance       dd      ?
 hWinMain        dd      ?
-hCursorMain     dd      ?
-hCursorMove     dd      ?
-hMenu           dd      ?
-hBmpBack        dd      ?
-hBmpClock       dd      ?
-; ç¼“å­˜çš„é’Ÿé¢èƒŒæ™¯
+; hCursorMain     dd      ?
+; hCursorMove     dd      ?
+; hMenu           dd      ?
+; hBmpBack        dd      ?
+; hBmpClock       dd      ?
+; »º´æµÄÖÓÃæ±³¾°
 hDCBack         dd      ?
-; ç¼“å­˜çš„æ¸¸æˆç”»é¢
+; »º´æµÄÓÎÏ·»­Ãæ
 hDCGame         dd      ?
-; æ¸¸æˆç›®æ ‡1
+; ÓÎÏ·Ä¿±ê1
 hDCObj1         dd      ?
-; ç¼“å­˜çš„å åŠ ä¸ŠæŒ‡é’ˆçš„é’Ÿé¢
-hDCClock        dd      ?
+; »º´æµÄµş¼ÓÉÏÖ¸ÕëµÄÖÓÃæ
+; hDCClock        dd      ?
 
 dwNowBack       dd      ?
-dwNowObj1       dd      ?
-dwNowCircle     dd      ?
+; dwNowObj1       dd      ?
+; dwNowCircle     dd      ?
 
 .const
-szClassName     db      'ASMå°æ¸¸æˆ', 0
-_dwPara180      dw      180
-dwRadius        dw      100/2
-szMenuBack1     db      'ä½¿ç”¨èƒŒæ™¯1(&A)', 0
-szMenuBack2     db      'ä½¿ç”¨èƒŒæ™¯2(&B)', 0
-szMenuCircle1   db      'ä½¿ç”¨è¾¹æ¡†1(&C)', 0
-szMenuCircle2   db      'ä½¿ç”¨è¾¹æ¡†2(&D)', 0
-szMenuExit      db      'é€€å‡º(&X)', 0
-debug_int       db      '%llu', 0ah, 0
+szClassName     db      'run away from covid-19', 0
+; _dwPara180      dw      180
+; dwRadius        dw      100/2
+; szMenuBack1     db      'Ê¹ÓÃ±³¾°1(&A)', 0
+; szMenuBack2     db      'Ê¹ÓÃ±³¾°2(&B)', 0
+; szMenuCircle1   db      'Ê¹ÓÃ±ß¿ò1(&C)', 0
+; szMenuCircle2   db      'Ê¹ÓÃ±ß¿ò2(&D)', 0
+; szMenuExit      db      'ÍË³ö(&X)', 0
+debug_int       db      '%d', 0ah, 0
 ; ########################################################## try code
 
 
 
 .code
-
-NextPos proc stdcall PtrtoBase :ptr byte, num :ptr dword, ptrlen :ptr dword
-
-
-
-
-NextPos endp
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ;1 create
-_create_ldf_need proc
-        local @playerPic
+; _create_ldf_need proc
+;         local @playerPic
 
-        invoke  GetDC, hWinMain
-        mov     @hDC, eax
-        invoke  CreateCompatibleDC, @hDC
-        mov     player.base.DC, eax
-        invoke  LoadBitmap, hInstance, dwPlayerPic
-        mov     @playerPic, eax
-        invoke SelectObject, player.base.DC, @playerPic 
-;2.1 maintain player, é‡åˆ°æ“ä½œæ—¶è°ƒç”¨
-_move_object_player proc
-_move_object_player endp
+;         invoke  GetDC, hWinMain
+;         mov     @hDC, eax
+;         invoke  CreateCompatibleDC, @hDC
+;         mov     player.base.DC, eax
+;         invoke  LoadBitmap, hInstance, dwPlayerPic
+;         mov     @playerPic, eax
+;         invoke SelectObject, player.base.DC, @playerPic 
+; ;2.1 maintain player, Óöµ½²Ù×÷Ê±µ÷ÓÃ
+; _move_object_player proc
+; _move_object_player endp
 
-;2.2 maintain bullet æ¯ä¸€å¸§è°ƒç”¨
-_move_object_bullet proc
-_move_object_bullet endp
+; ;2.2 maintain bullet Ã¿Ò»Ö¡µ÷ÓÃ
+; _move_object_bullet proc
+; _move_object_bullet endp
 
-;2.3 maintain all object  æ¯ä¸€å¸§è°ƒç”¨
-_move_object_obj proc
-_move_object_obj endp
+; ;2.3 maintain all object  Ã¿Ò»Ö¡µ÷ÓÃ
+; _move_object_obj proc
+; _move_object_obj endp
 
-;3 check collision æ¯ä¸€å¸§è°ƒç”¨
-_check_collision proc
-;ä¸¤ä¸¤æšä¸¾æ‰€æœ‰ç‰©ä½“ï¼Œä»¤é‚£äº›åº”è¯¥æ¶ˆå¤±çš„æ¶ˆå¤±ï¼š
-;åŒæ—¶ç»´æŠ¤targetsæ•°ç»„çš„æ•°é‡target_number
-_check_collision endp
+; ;3 check collision Ã¿Ò»Ö¡µ÷ÓÃ
+; _check_collision proc
+; ;Á½Á½Ã¶¾ÙËùÓĞÎïÌå£¬ÁîÄÇĞ©Ó¦¸ÃÏûÊ§µÄÏûÊ§£º
+; ;Í¬Ê±Î¬»¤targetsÊı×éµÄÊıÁ¿target_number
+; _check_collision endp
 
 
+_set_char_pos proc
+        mov game_start.posx, 70
+        mov game_start.posy, 50
+        mov game_start.lengthx, charH
+        mov game_start.lengthy, charW
+        mov game_start_chosen.posx, 70
+        mov game_start_chosen.posy, 50
+        mov game_start_chosen.lengthx, charH
+        mov game_start_chosen.lengthy, charW
 
+        mov game_intro.posx, 70
+        mov game_intro.posy, 180
+        mov game_intro.lengthx, charH
+        mov game_intro.lengthy, charW
+        mov game_intro_chosen.posx, 70
+        mov game_intro_chosen.posy, 180
+        mov game_intro_chosen.lengthx, charH
+        mov game_intro_chosen.lengthy, charW
+
+        ret
+_set_char_pos endp
 _createAll proc 
         local   @hDC, @hDCCircle, @hDCMask
-        local   @hBmpBack, @hBmpObj1, @hBmpMask
+        local   @hBmpBack, @hBmpObj1, @hBmpObj2, @hBmpObj3
+        local   @hBmpGS, @hBmpGI
         local   @brush
 
         mov     dwNowBack, IDB_BACKG
-        mov     dwNowObj1, IDB_OBJ1
         invoke  GetDC, hWinMain
         mov     @hDC, eax
 
         invoke  CreateCompatibleDC, @hDC
         mov     hDCBack, eax
-        invoke  CreateCompatibleDC, @hDC
-        mov     hDCObj1, eax
+
         invoke  CreateCompatibleDC, @hDC
         mov     hDCGame, eax
         invoke  CreateCompatibleBitmap, @hDC, gameH, gameW
-        mov     hBmpBack, eax
-        invoke  SelectObject, hDCGame, hBmpBack ; set draw area to DC
+        mov     @hBmpBack, eax
+        invoke  SelectObject, hDCGame, @hBmpBack ; set draw area to DC
+
 
         invoke  LoadBitmap, hInstance, dwNowBack
-        mov     @hBmpBack, eax ;load back pic
-        invoke  LoadBitmap, hInstance, dwNowObj1
-        mov     @hBmpObj1, eax ;load obj pic
+        mov     @hBmpBack, eax ;load back
 
-
-        invoke SelectObject, hDCBack, @hBmpBack
-        ; invoke  CreatePatternBrush, @hBmpBack
-        ; mov     @brush, eax
-        ; invoke  SelectObject, hDCBack, @brush
-        ; invoke  PatBlt, hDCBack, 0, 0, gameH, gameW, PATCOPY ;draw  background
-        invoke  SelectObject, hDCObj1, @hBmpObj1
-
-        invoke  ReleaseDC, hWinMain, @hDC
+        invoke  SelectObject, hDCBack, @hBmpBack
         ; invoke  DeleteObject, @brush
         invoke  DeleteObject, @hBmpBack
+;Èı¸ö»·¾³Í¼Ïñ
+        invoke  CreateCompatibleDC, @hDC
+        mov     env_objecet_1.DC, eax
+        invoke  CreateCompatibleDC, @hDC
+        mov     env_objecet_2.DC, eax
+        invoke  CreateCompatibleDC, @hDC
+        mov     env_objecet_3.DC, eax
+        invoke  LoadBitmap, hInstance, IDB_OBJ1
+        mov     @hBmpObj1, eax ;load obj
+        invoke  LoadBitmap, hInstance, IDB_OBJ2
+        mov     @hBmpObj2, eax ;load obj
+        invoke  LoadBitmap, hInstance, IDB_OBJ3
+        mov     @hBmpObj3, eax ;load obj
+        invoke  SelectObject, env_objecet_1.DC, @hBmpObj1
+        invoke  SelectObject, env_objecet_2.DC, @hBmpObj2
+        invoke  SelectObject, env_objecet_3.DC, @hBmpObj3
         invoke  DeleteObject, @hBmpObj1
+        invoke  DeleteObject, @hBmpObj2
+        invoke  DeleteObject, @hBmpObj3
+;¿ªÊ¼ÎÄ×Ö
+        invoke  CreateCompatibleDC, @hDC
+        mov     game_start.DC, eax
+        invoke  CreateCompatibleDC, @hDC
+        mov     game_intro.DC, eax
+        invoke  LoadBitmap, hInstance, IDB_GAMESTART
+        mov     @hBmpGS, eax ;load obj
+        invoke  LoadBitmap, hInstance, IDB_GAMEINTRO
+        mov     @hBmpGI, eax ;load obj
+        invoke  SelectObject, game_start.DC, @hBmpGS
+        invoke  SelectObject, game_intro.DC, @hBmpGI
+        invoke  DeleteObject, @hBmpGS
+        invoke  DeleteObject, @hBmpGI
 
-        invoke create_ldf_need
-        invoke create_lja_need
-        invoke create_sst_need
+        invoke  CreateCompatibleDC, @hDC
+        mov     game_start_chosen.DC, eax
+        invoke  CreateCompatibleDC, @hDC
+        mov     game_intro_chosen.DC, eax
+        invoke  LoadBitmap, hInstance, IDB_GAMESTARTC
+        mov     @hBmpGS, eax ;load obj
+        invoke  LoadBitmap, hInstance, IDB_GAMEINTROC
+        mov     @hBmpGI, eax ;load obj
+        invoke  SelectObject, game_start_chosen.DC, @hBmpGS
+        invoke  SelectObject, game_intro_chosen.DC, @hBmpGI
+        invoke  DeleteObject, @hBmpGS
+        invoke  DeleteObject, @hBmpGI
+
+        invoke _set_char_pos
+
+;
+        invoke  ReleaseDC, hWinMain, @hDC
         ret 
 _createAll endp
 
@@ -232,25 +291,98 @@ _Quit           proc
         ret
 _Quit           endp
 
-_move_object proc
+_move_object proc hWnd
+        local @mouse:POINT
+        local @window:RECT
+
+
+
         invoke  BitBlt, hDCGame, 0, 0, gameH, gameW, hDCBack, 0, 0, SRCCOPY
-        mov eax, object1H
-        mov ebx, object1W
-        inc eax
-        inc ebx
-        cmp eax, object1H_end
-        jl continue
-        cmp ebx, object1W_end
-        jl continue
-        mov eax, object1H_begin
-        mov ebx, object1W_begin
-continue:
-        mov object1H, eax
-        mov object1W, ebx
-        ; invoke  StretchBlt, hDCGame, 100, 100, eax, ebx, 
-        ;                     hDCObj1, 0,  0,  object1H_init, object1W_init, SRCCOPY
-        invoke  TransparentBlt, hDCGame, 100, 100, eax, ebx, 
-                                hDCObj1, 0,  0,  object1H_init, object1W_init, 16777215
+        mov eax, cur_interface
+        .if eax == in_begining 
+                mov choose_which_char, 0
+
+                invoke	GetCursorPos,addr @mouse
+                invoke  GetWindowRect, hWnd, addr @window
+                invoke printf, offset debug_int, @mouse.x
+                mov eax, @window.left
+                mov ebx, @window.top
+                ; sub @mouse.x, windows
+                add eax, game_start.posx
+                ; invoke printf, offset debug_int, eax
+                add ebx, game_start.posy
+                .if (eax <= @mouse.x) && (ebx <= @mouse.y)
+                        add eax, game_start.lengthx
+                        add ebx, game_start.lengthy
+                        .if (eax >= @mouse.x) && (ebx >= @mouse.y)
+                                mov choose_which_char, choose_start
+                        .endif
+                .endif
+
+                mov eax, @window.left
+                mov ebx, @window.top
+                add eax, game_intro.posx
+                add ebx, game_intro.posy
+                .if (eax <= @mouse.x) && (ebx <= @mouse.y)
+                        add eax, game_intro.lengthx
+                        add ebx, game_intro.lengthy
+                        .if (eax >= @mouse.x) && (ebx >= @mouse.y)
+                                mov choose_which_char, choose_intro
+                        .endif
+                .endif
+                invoke printf, offset debug_int, choose_which_char
+
+                mov eax, choose_which_char
+                .if eax == choose_start
+                        invoke  TransparentBlt, 
+                                hDCGame, 
+                                game_start_chosen.posx, game_start_chosen.posy, charH, charW, 
+                                game_start_chosen.DC, 
+                                0, 0, game_start_chosen.lengthx, game_start_chosen.lengthy, 
+                                16777215               
+                .else
+                        invoke  TransparentBlt, 
+                                hDCGame, 
+                                game_start.posx, game_start.posy, charH, charW, 
+                                game_start.DC, 
+                                0, 0, game_start.lengthx, game_start.lengthy, 
+                                16777215  
+                .endif
+
+                mov eax, choose_which_char
+                .if eax == choose_intro
+                        invoke  TransparentBlt, 
+                                hDCGame, 
+                                game_intro_chosen.posx, game_intro_chosen.posy, charH, charW, 
+                                game_intro_chosen.DC, 
+                                0, 0, game_intro_chosen.lengthx, game_intro_chosen.lengthy, 
+                                16777215     
+                .else
+                        invoke  TransparentBlt, 
+                                hDCGame, 
+                                game_intro.posx, game_intro.posy, charH, charW, 
+                                game_intro.DC, 
+                                0, 0, game_intro.lengthx, game_intro.lengthy, 
+                                16777215  
+                .endif
+        .endif
+;         mov eax, object1H
+;         mov ebx, object1W
+;         inc eax
+;         inc ebx
+;         cmp eax, object1H_end
+;         jl continue
+;         cmp ebx, object1W_end
+;         jl continue
+;         mov eax, object1H_begin
+;         mov ebx, object1W_begin
+; continue:
+;         mov object1H, eax
+;         mov object1W, ebx
+;         ; invoke  StretchBlt, hDCGame, 100, 100, eax, ebx, 
+;         ;                     hDCObj1, 0,  0,  object1H_init, object1W_init, SRCCOPY
+;         invoke  TransparentBlt, hDCGame, 10, 10, eax, ebx, 
+;                                 env_objecet_3.DC, 0,  0,  object1H_init, object1W_init, 16777215
         ret
 _move_object endp
 _ProcWinMain    proc    uses ebx edi esi, hWnd, uMsg, wParam, lParam
@@ -267,8 +399,8 @@ _ProcWinMain    proc    uses ebx edi esi, hWnd, uMsg, wParam, lParam
                 sub     eax, @stPs.rcPaint.left
                 mov     ecx, @stPs.rcPaint.bottom
                 sub     ecx, @stPs.rcPaint.top
-                ; å¤åˆ¶ç¼“å­˜çš„é’Ÿé¢å›¾ç‰‡åˆ°æ˜¾ç¤ºDC
-                ; è‹¥1ç§’å¤šæ¬¡æ”¶åˆ°WM_PAINTä¸å¿…é‡å¤è®¡ç®—
+                ; ¸´ÖÆ»º´æµÄÖÓÃæÍ¼Æ¬µ½ÏÔÊ¾DC
+                ; Èô1Ãë¶à´ÎÊÕµ½WM_PAINT²»±ØÖØ¸´¼ÆËã
                 invoke  BitBlt, @hDC, @stPs.rcPaint.left, @stPs.rcPaint.top, eax, ecx, hDCGame, @stPs.rcPaint.left, @stPs.rcPaint.top, SRCCOPY
                 invoke  EndPaint, hWnd, addr @stPs
         .elseif eax == WM_CREATE
@@ -280,10 +412,21 @@ _ProcWinMain    proc    uses ebx edi esi, hWnd, uMsg, wParam, lParam
         .elseif eax == WM_TIMER
                 mov     eax, wParam
                 ; .if     eax == ID_TIMER1
-                invoke  _move_object
+                invoke  _move_object, hWnd
                 ; .elseif eax == ID_TIMER2
                 invoke  InvalidateRect, hWnd, NULL, FALSE
 
+        .elseif eax == WM_LBUTTONUP
+                mov eax, cur_interface
+                .if eax == in_begining 
+                        mov eax, choose_which_char
+                        .if eax == choose_start
+                                mov cur_interface, in_game
+                        .endif
+                        .if eax == choose_intro
+                                
+                        .endif
+                .endif
 
         .elseif eax == WM_CLOSE
                 invoke  _Quit
@@ -299,31 +442,31 @@ _WinMain        proc
                 local   @stWndClass: WNDCLASSEX
                 local   @stMsg: MSG
 
-        ; æ¨¡å—å¥æŸ„
+        ; Ä£¿é¾ä±ú
         invoke  GetModuleHandle, NULL
         mov     hInstance, eax
 
-        ; ç»“æ„ä½“æ¸…é›¶
+        ; ½á¹¹ÌåÇåÁã
         ; invoke  LoadCursor, hInstance, IDC_MOVE
         ; mov     hCursorMove, eax
         ; invoke  LoadCursor, hInstance, IDC_MAIN
         ; mov     hCursorMain, eax
         invoke  RtlZeroMemory, addr @stWndClass, sizeof @stWndClass
-                ; invoke  LoadIcon, hInstance, IDB_ICON
-                ; mov     @stWndClass.hIcon, eax ; è®¾ç½®å°å›¾æ ‡
-                ; mov     @stWndClass.hIconSm, eax
+        invoke  LoadIcon, hInstance, IDB_ICON
+        mov     @stWndClass.hIcon, eax ; ÉèÖÃĞ¡Í¼±ê
+        mov     @stWndClass.hIconSm, eax
         invoke  LoadCursor, 0, IDC_ARROW
         mov     @stWndClass.hCursor, eax
         mov     eax, hInstance
         mov     @stWndClass.hInstance, eax
         mov     @stWndClass.cbSize, sizeof WNDCLASSEX
         mov     @stWndClass.style, CS_HREDRAW or CS_VREDRAW
-        ; æ³¨å†Œçª—å£ç±»æ—¶æŒ‡å®šå¯¹åº”çš„çª—å£è¿‡ç¨‹
+        ; ×¢²á´°¿ÚÀàÊ±Ö¸¶¨¶ÔÓ¦µÄ´°¿Ú¹ı³Ì
         mov     @stWndClass.lpfnWndProc, offset _ProcWinMain
         mov     @stWndClass.hbrBackground, COLOR_WINDOW + 1
         mov     @stWndClass.lpszClassName, offset szClassName
-        ; æ³¨å†Œçª—å£ç±»
-        ; æ³¨æ„åŒä¸€çª—å£ç±»çš„çª—å£éƒ½å…·æœ‰ç›¸åŒçš„çª—å£è¿‡ç¨‹
+        ; ×¢²á´°¿ÚÀà
+        ; ×¢ÒâÍ¬Ò»´°¿ÚÀàµÄ´°¿Ú¶¼¾ßÓĞÏàÍ¬µÄ´°¿Ú¹ı³Ì
         invoke  RegisterClassEx, addr @stWndClass
         invoke  CreateWindowEx, NULL, \
                 offset szClassName, offset szClassName, \
