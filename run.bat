ee79a735e59a1251d0c86daacc16f489ea972066
smzzl <smzzl>
2022-12-04 00:39:56 +0800

    new file:   main.RES 	modified:   main.exe 	modified:   run.bat

--- run.bat
+++ run.bat
@@ -5,10 +5,5 @@ set include=%Masm32Dir%\Include;%include%
 set lib=%Masm32Dir%\lib;%lib%
 set path=%Masm32Dir%\Bin;%Masm32Dir%;%PATH%
 nmake clean
-<<<<<<< HEAD
-=======
-@REM nmake ldf_dev
-@REM nmake main_dev
->>>>>>> 977e3333c03cba73d62b684ebc75ffc4560e14a8
 nmake
 
