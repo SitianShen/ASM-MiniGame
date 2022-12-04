4025aaffb901fba99cc44bfa290cb9d14db961b5
林东方 <10892577+lin-dongfang@user.noreply.gitee.com>
2022-12-04 14:35:21 +0800

    modified:   data.asm 	modified:   dev/ldf_dev.asm 	modified:   dev/lja_dev.asm 	modified:   dev/sst_dev.asm 	modified:   dev/zzl_dev.asm 	new file:   global/global_public.inc 	modified:   release/ldf.asm 	modified:   release/lja.asm 	modified:   release/sst.asm 	modified:   release/zzl.asm

--- data.asm
+++ data.asm
@@ -3,41 +3,6 @@
 option casemap: none
 
 include global.inc
-
-public player         
-public bullet             
-public targets
-
-;zzl own
-public button_play      
-public button_start      
-public button_back      
-public button_exit       
-public backGround       
-public object_DC        
-
-public hInstance       
-public hWinMain      
-public hDCBack         
-public hDCGame         
-public hDCObj1        
-public dwNowBack     
-
-public flag_jump               
-public flag_movleft          
-public flag_movright           
-public stdtime_jump          
-public time_jump              
-public stdtime_mov             
-public time_mov             
-public cur_interface     
-
-public target_number       
-public speed
-
-public object1H   
-public object1W      
-public object_move_v  
-public POSCNT
+include global_public.inc
 
 end
