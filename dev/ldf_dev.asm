65386f58204e65cd233dbf5f2e9643cc95fce237
林东方 <10892577+lin-dongfang@user.noreply.gitee.com>
2022-12-07 10:28:26 +0800

    modified:   dev/ldf_dev.asm 	modified:   release/ldf.asm

--- dev/ldf_dev.asm
+++ dev/ldf_dev.asm
@@ -327,7 +327,8 @@ _collision_Player_with_HARD proc uses esi,  HARD_target:ptr Targets
         lea esi, player
         assume esi:ptr Subject
         
-        mov [esi].base.alive, 0
+        ; mov [esi].base.alive, 0
+        dec [esi].base.alive
         ; invoke printf, offset debug_int, [esi].base.posx
         ; invoke printf, offset debug_int, [esi].base.posy
         invoke printf, offset debug_str, offset szCarWithHard
