9d493e8f9a86e3f57148827deab4c2c292b7d7c2
Sitian Shen <1215665036@qq.com>
2022-12-13 17:51:31 +0800

    modified:   dev/sst_dev.asm 	modified:   global/global_const.inc

--- run.bat
+++ run.bat
@@ -7,11 +7,11 @@ set path=%Masm32Dir%\Bin;%Masm32Dir%;%PATH%;
 
 
 nmake clean
-@REM nmake main && main.exe
+nmake main && main.exe
 nmake ldf_dev && ldf_dev.exe
-@REM nmake sst_dev && sst_dev.exe
-@REM nmake zzl_dev && zzl_dev.exe
-@REM nmake lja_dev && lja_dev.exe
+nmake sst_dev && sst_dev.exe
+nmake zzl_dev && zzl_dev.exe
+nmake lja_dev && lja_dev.exe
 
 
 
