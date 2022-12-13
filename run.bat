6ce32798a6be7ce6a58029785d3b6e10b410c132
林东方 <10892577+lin-dongfang@user.noreply.gitee.com>
2022-12-13 15:25:38 +0800

    modified:   dev/sst_dev.asm 	modified:   run.bat 	modified:   release/sst.asm 	modified:   run.bat

--- run.bat
+++ run.bat
@@ -7,13 +7,8 @@ set path=%Masm32Dir%\Bin;%Masm32Dir%;%PATH%;
 
 
 nmake clean
-<<<<<<< HEAD
 @REM nmake main && main.exe
 nmake ldf_dev && ldf_dev.exe
-=======
-nmake main
-@REM nmake ldf_dev && ldf_dev.exe
->>>>>>> f48b71083e3ff5877dc5fd7b1c7cf75ee878cf66
 @REM nmake sst_dev && sst_dev.exe
 @REM nmake zzl_dev && zzl_dev.exe
 @REM nmake lja_dev && lja_dev.exe
