f48b71083e3ff5877dc5fd7b1c7cf75ee878cf66
GoodMorningPeter <shuxuejianmo202202@163.com>
2022-12-13 14:35:25 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -7,13 +7,8 @@ set path=%Masm32Dir%\Bin;%Masm32Dir%;%PATH%;
 
 
 nmake clean
-<<<<<<< HEAD
-@REM nmake main
-nmake ldf_dev && ldf_dev.exe
-=======
 nmake main
 @REM nmake ldf_dev && ldf_dev.exe
->>>>>>> d2899d3bd86f2278953672d6b29824ea03b5ebf7
 @REM nmake sst_dev && sst_dev.exe
 @REM nmake zzl_dev && zzl_dev.exe
 @REM nmake lja_dev && lja_dev.exe
